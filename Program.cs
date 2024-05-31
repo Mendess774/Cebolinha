613ae4429d75078ca4371ebdb465cadab1b5ef92
Felipe Mendes <you@example.com>
2024-05-31 15:15:44 -0300

    exelcicio finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string? frase;
+Console.Write("Digite uma flase:");
+frase = Console.ReadLine();
+
+string? FraseComDislalia = frase?
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine(FraseComDislalia);
